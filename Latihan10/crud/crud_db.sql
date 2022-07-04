559197dcaec006693e47fa63c9e1a19ea5407b09
Zubreknov <farhanandono@gmail.com>
2022-07-04 09:25:02 +0700

    Add files via upload

---
+++ Latihan10/crud/crud_db.sql
@@ -0,0 +1,15 @@
+--
+-- Database: 'crud_db'
+--
+
+-- -------------------------------------------------------
+
+--
+-- Struktur dari tabel 'crud'
+--
+CREATE TABLE 'crud' (
+	'id' int(11) NOT NULL,
+	'name' varchar(100) NOT NULL,
+	'email' varchar(100) NOT NULL,
+	'mobile' varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
