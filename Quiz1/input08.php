8b19912fd1a0740e51a9545024f555ad84b31d16
Zubreknov <farhanandono@gmail.com>
2022-07-04 07:51:06 +0700

    Add files via upload

---
+++ Quiz1/input08.php
@@ -0,0 +1,17 @@
+<html>
+	<head><title>Film Kartun Favorit ~ Inputan Combo</title></head>
+	<body>
+		<FORM ACTION="proses07.php" METHOD="POST" NAME="input">
+			<h2>Pilih Film Kartun Favorit Anda :</h2>
+			<select name="Kartun">
+				<option value="Sponge Bob">Sponge Bob</option>
+				<option value="Sinchan">Sinchan</option>
+				<option value="Conan">Conan</option>
+				<option value="Doreaemon">Doreaemon</option>
+				<option value="Dragon Ball">Dragon Ball</option>
+				<option value="Naruto">Naruto</option>
+			</select>
+			<input type="submit" name="Pilih" value="Pilih">
+		</FORM>
+	</body>
+</html>
